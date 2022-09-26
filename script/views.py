6a7d9bd4f6d3615091d879f9c39e1369c745d865
SOOYEON YEON <dustndus8@naver.com>
2022-09-26 15:54:45 +0900

    feat: create, get 작성

--- script/views.py
+++ script/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.db.models import Q
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from script.forms import ScriptForm
+from script.models import Script
+
+
+def createScript(request):
+    if request.method == "GET":
+        scriptForm = ScriptForm(request.POST)
+        context = {'scriptForm' : scriptForm}
+        return render(request, 'script/create.html',context)
+    if request.method == "POST":
+        scriptForm = ScriptForm(request.POST)
+        if scriptForm.is_valid():
+            script = scriptForm.save(commit=False)
+            script.save()
+
+        return redirect('/script/readGet/'+str(script.id))
+
+
+    return render(request, 'script/create.html')
+
+def readScriptGet(request,sid):
+    script = Script.objects.get(Q(id=sid))
+    context = {
+        'script' : script
+    }
+
+    return render(request, 'script/read.html',context)
+
