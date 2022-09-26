b1e5213392dcd6dc1c705d729cf7cc282cfc76be
SOOYEON YEON <dustndus8@naver.com>
2022-09-27 08:55:54 +0900

    chore: 배포 설정 변경

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ with open(secret_files) as f:
 SECRET_KEY = secrets['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
