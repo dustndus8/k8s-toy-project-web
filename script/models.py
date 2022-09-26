f52cd6f7065ec686fc9dc1f5809f0f426b059851
SOOYEON YEON <dustndus8@naver.com>
2022-09-26 14:38:09 +0900

    feat: model 작성

--- script/models.py
+++ script/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+FLAVOR_CHOICES = (
+    ('x1.small','X1.SMALL'),
+    ('x1.medium','X1.MEDIUM'),
+    ('x1.large','X1.LARGE'),
+)
+
+class Script(models.Model):
+    project_name = models.CharField(max_length=40)
+    key_pair_name = models.CharField(max_length=40)
+    flavor = models.CharField(choices=FLAVOR_CHOICES, default='x1.small')
+    worker = models.IntegerField()
