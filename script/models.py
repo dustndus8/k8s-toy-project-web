588d2960da9c7cb2cc01a615bdb572b6fd4d4bc1
SOOYEON YEON <dustndus8@naver.com>
2022-09-26 15:54:12 +0900

    refactor: 모델 수정

--- script/models.py
+++ script/models.py
@@ -7,8 +7,16 @@ FLAVOR_CHOICES = (
     ('x1.large','X1.LARGE'),
 )
 
+IMAGE_CHOICES = (
+    ('centos','CENTOS'),
+    ('ubuntu','UBUNTU'),
+)
+
+
 class Script(models.Model):
     project_name = models.CharField(max_length=40)
     key_pair_name = models.CharField(max_length=40)
-    flavor = models.CharField(choices=FLAVOR_CHOICES, default='x1.small')
-    worker = models.IntegerField()
+    flavor = models.CharField(max_length=40,choices=FLAVOR_CHOICES, default='x1.small')
+    image = models.CharField(max_length=40,choices=IMAGE_CHOICES,default='ubuntu')
+    worker = models.IntegerField(default=0)
+    create_date = models.DateTimeField(auto_now_add=True)
