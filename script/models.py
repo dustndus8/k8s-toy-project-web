36ea746a0628599542faeaa0c17b73bf5382c2d5
SOOYEON YEON <dustndus8@naver.com>
2022-10-04 17:25:43 +0900

    chore: image choice 조정

--- script/models.py
+++ script/models.py
@@ -10,7 +10,6 @@ FLAVOR_CHOICES = (
 
 IMAGE_CHOICES = (
     ('centos','CENTOS'),
-    ('ubuntu','UBUNTU'),
 )
 
 
@@ -18,7 +17,7 @@ class Script(models.Model):
     project_name = models.CharField(max_length=40)
     key_pair_name = models.CharField(max_length=40)
     flavor = models.CharField(max_length=40,choices=FLAVOR_CHOICES, default='x1.small')
-    image = models.CharField(max_length=40,choices=IMAGE_CHOICES,default='ubuntu')
+    image = models.CharField(max_length=40,choices=IMAGE_CHOICES,default='centos')
     worker = models.IntegerField(default=0)
     writer = models.ForeignKey(User,on_delete=models.CASCADE)
     create_date = models.DateTimeField(auto_now_add=True)
